e20f6707afd0f6c62860e0289795f4520a3d1c2d
brianondemand <snowworksdev@gmail.com>
2024-07-30 08:27:53 +0300

    Initial Commit

---
+++ app/components/HomeBanner.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+
+const HomeBanner = () => {
+  return (
+    <div className="relative bg-gradient-to-r from-sky-700 to-sky-900 mb-8">
+      <div className="mx-auto px-8 py-12 flex flex-col gap-2 md:flex-row items-center justify-evenly">
+        <div className="mb-8 md:mb-0 text-center">
+          <h1 className="text-4xl md:text-6xl font-bold text-white mb-4">
+            CASE YOUR PHONE
+          </h1>
+          <p className="text-xl md:text-2xl text-yellow-300">
+            Impossibly thin phone cases
+          </p>
+        </div>
+        <div className="w-1/3 relative aspect-video">
+          <Image src="/casePoa.webp" alt="" fill className="object-contain" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomeBanner;
---
+++ app/components/footer/Footer.tsx
@@ -0,0 +1,55 @@
+import Link from "next/link";
+import Container from "../Container";
+import FooterList from "./FooterList";
+import { MdFacebook } from "react-icons/md";
+import { AiFillInstagram, AiFillTwitterCircle } from "react-icons/ai";
+
+const Footer = () => {
+  return (
+    <footer className="bg-slate-700 text-slate-200 text-sm mt-16">
+      <Container>
+        <div className="flex flex-col md:flex-row justify-between pt-16 pb-8">
+          <FooterList>
+            <h3 className="text-base font-bold mb-2">Shop Categories</h3>
+            <Link href="#">Rolling Papers</Link>
+            <Link href="#">Zip Locks</Link>
+            <Link href="#">Grinders</Link>
+            <Link href="#">Bongs</Link>
+            <Link href="#">Filter Papers</Link>
+            <Link href="#">Ash Trays</Link>
+          </FooterList>
+          <FooterList>
+            <h3 className="text-base font-bold mb-2">Customer Service</h3>
+            <Link href="#">Contact Us</Link>
+            <Link href="#">Shipping Policy</Link>
+            <Link href="#">Returns & Exchange</Link>
+            <Link href="#">FAQs</Link>
+          </FooterList>
+          <div className="w-full md:w-1/3 mb-6 md:mb-0">
+            <h3 className="text-base font-bold mb-2">About Us</h3>
+            <p className="mb-2">
+              Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+              commodo ligula eget dolor. Aenean massa.
+            </p>
+          </div>
+          <FooterList>
+            <h3 className="text-base font-bold mb-2">Follow Us</h3>
+            <div className="flex gap-2">
+              <Link href="#">
+                <MdFacebook size={24} />
+              </Link>
+              <Link href="#">
+                <AiFillTwitterCircle size={24} />
+              </Link>
+              <Link href="#">
+                <AiFillInstagram size={24} />
+              </Link>
+            </div>
+          </FooterList>
+        </div>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
